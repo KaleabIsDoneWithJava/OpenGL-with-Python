0019616b8610adc9b417dd464dcd17dbd6bb51c0
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2023-07-21 11:09:16 +0300

    For the newer opengl >=3.1 (Prgrammable Pipeline)

--- ep02_fixed_function_pipeline.py
+++ ep02_fixed_function_pipeline.py
@@ -1,73 +1,96 @@
 import glfw
 from OpenGL.GL import *
+import OpenGL.GL.shaders
 import numpy as np
 
-# initializing glfw library
+# Vertex and color data
+vertices = np.array([-0.5, -0.5, 0.0,  
+                     0.5, -0.5, 0.0, 
+                     0.0, 0.5, 0.0], dtype=np.float32)
+                     
+colors = np.array([1.0, 0.0, 0.0,  
+                   0.0, 1.0, 0.0,
+                   0.0, 0.0, 1.0], dtype=np.float32)
+
+# Shader source code
+vertex_src = """
+# version 330
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec3 a_color;
+
+out vec3 v_color;
+
+void main()
+{
+    gl_Position = vec4(a_position, 1.0);
+    v_color = a_color;
+}
+"""
+
+fragment_src = """
+# version 330
+in vec3 v_color;
+out vec4 out_color;
+
+void main()
+{
+    out_color = vec4(v_color, 1.0);
+}
+"""
+
+# Initialize glfw  
 if not glfw.init():
     raise Exception("glfw can not be initialized!")
 
-# Configure the OpenGL context.
-# If we are planning to use anything above 2.1 we must at least
-# request a 3.3 core context to make this work across platforms.
+# Configure OpenGL context
 glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
 glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
 glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
-# 4 MSAA is a good default with wide support
-glfw.window_hint(glfw.SAMPLES, 4)
 
-# creating the window
+# Create window
 window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
-
-# check if window was created
 if not window:
     glfw.terminate()
     raise Exception("glfw window can not be created!")
 
-# Query the actual framebuffer size so we can set the right viewport later
-# -> glViewport(0, 0, framebuffer_size[0], framebuffer_size[1])
-framebuffer_size = glfw.get_framebuffer_size(window)
-
-# set window's position
-glfw.set_window_pos(window, 400, 200)
-
-# make the context current
 glfw.make_context_current(window)
 
-vertices = [-0.5, -0.5, 0.0, 
-            0.5, -0.5, 0.0,
-            0.0, 0.5, 0.0]
+# Compile and link shaders
+vertex_shader = OpenGL.GL.shaders.compileShader(vertex_src, GL_VERTEX_SHADER)
+fragment_shader = OpenGL.GL.shaders.compileShader(fragment_src, GL_FRAGMENT_SHADER)
+shader_program = OpenGL.GL.shaders.compileProgram(vertex_shader, fragment_shader)
 
-colors = [1.0, 0.0, 0.0,
-          0.0, 1.0, 0.0,
-          0.0, 0.0, 1.0]
+# Vertex Array Object
+VAO = glGenVertexArrays(1)
+glBindVertexArray(VAO)
 
-#Converting them into an array because we can't use python lists in OpenGL
-#For some unkown reason
-vertices = np.array(vertices, dtype=np.float32)
-colors = np.array(colors, dtype=np.float32)
+# Vertex Buffer Object
+VBO = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, VBO)
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+glEnableVertexAttribArray(0) 
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, ctypes.c_void_p(0))
 
-glEnableClientState(GL_VERTEX_ARRAY)
-glVertexPointer(3, GL_FLOAT, 0, vertices)
+# Color Buffer Object
+CBO = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, CBO)
+glBufferData(GL_ARRAY_BUFFER, colors.nbytes, colors, GL_STATIC_DRAW)
+glEnableVertexAttribArray(1)
+glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, ctypes.c_void_p(0))
 
+glUseProgram(shader_program)
 
-
-glEnableClientState(GL_COLOR_ARRAY)
-glColorPointer(3, GL_FLOAT, 0, colors)
-
-#glDisableClientState()
-
-#Dark greenish blue
-glClearColor(0, 0.5, 0.5, 1)
-
-
-
-# the main application loop
+# Main render loop
 while not glfw.window_should_close(window):
     glfw.poll_events()
+    
+    glClearColor(0.2, 0.2, 0.2, 1) 
     glClear(GL_COLOR_BUFFER_BIT)
+
+    glBindVertexArray(VAO)
     glDrawArrays(GL_TRIANGLES, 0, 3)
+
     glfw.swap_buffers(window)
 
-# terminate glfw, free up allocated resources
+# Terminate glfw
 glfw.terminate()
